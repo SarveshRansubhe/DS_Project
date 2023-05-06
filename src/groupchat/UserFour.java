c139b24c43da7f516015a83e284e627ecee5316f
sarvesh <>
2023-05-06 15:42:20 +0530

    first

---
+++ src/groupchat/UserFour.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package groupchat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+public class UserFour implements ActionListener, Runnable {
+
+    JTextField text;
+    JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+    static DataOutputStream dout;
+
+    BufferedReader reader;
+    BufferedWriter writer;
+    String name = "User Four";
+
+    UserFour() {
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(3, 61, 255));
+        p1.setBounds(0, 0, 450, 70);
+        f.add(p1);
+
+        JLabel status = new JLabel("DS Project User 4");
+        status.setBounds(110, 15, 100, 18);
+        status.setForeground(Color.WHITE);
+        status.setFont(new Font("SAN_SHERIF", Font.BOLD, 20));
+        p1.add(status);
+
+        a1 = new JPanel();
+        a1.setBounds(5, 75, 440, 570);
+        f.add(a1);
+
+        text = new JTextField();
+        text.setBounds(4, 655, 310, 40);
+        f.add(text);
+
+        JButton send = new JButton("Send");
+        send.setBounds(320, 655, 123, 40);
+        send.addActionListener(this);
+        f.add(send);
+
+        f.setSize(465, 750);
+        f.getContentPane().setBackground(Color.GRAY);
+        f.setVisible(true);
+
+        try {
+            Socket socket = new Socket("localhost", 2003);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            String out = "<html><p>" + name + "</p><p>" + text.getText() + "</p> </html>";
+
+            JPanel p2 = formatLabel(out);
+
+            a1.setLayout(new BorderLayout());
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            a1.add(vertical, BorderLayout.PAGE_START);
+
+            try {
+                writer.write(out);
+                writer.write("\r\n");
+                writer.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            text.setText("");
+            f.repaint();
+            f.invalidate();
+            f.validate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JPanel formatLabel(String out) {
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel output = new JLabel(out);
+
+        panel.add(output);
+
+        return panel;
+    }
+
+    public void run() {
+        try {
+            String msg = "";
+            while (true) {
+                msg = reader.readLine();
+                if (msg.contains(name)) {
+                    continue;
+                }
+                JPanel panel = formatLabel(msg);
+                JPanel left = new JPanel(new BorderLayout());
+
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
+
+                a1.add(vertical, BorderLayout.PAGE_START);
+                f.repaint();
+                f.invalidate();
+                f.validate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        UserFour four = new UserFour();
+        Thread t1 = new Thread(four);
+        t1.start();
+    }
+}
